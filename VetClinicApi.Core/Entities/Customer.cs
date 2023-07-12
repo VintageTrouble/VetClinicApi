2863bf0240f9eb7bdb3667f5f8851103f56d7a85
Dmitrii Korpachev <mrdecart@yandex.ru>
2023-07-12 19:21:52 +0700

    Create Database connection + add first migration

---
+++ VetClinicApi.Core/Entities/Customer.cs
@@ -0,0 +1,18 @@
+﻿
+namespace VetClinicApi.Core.Entities
+{
+    public class Customer : IEntity
+    {
+        public int Id { get; set; }
+        public string PassportNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public DateTime LastEditDate { get; set; }
+        public DateTime? LastVisitDate { get; set; }
+
+    }
+
+}
