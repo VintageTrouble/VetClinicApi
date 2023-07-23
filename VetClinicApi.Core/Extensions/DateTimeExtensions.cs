9f3d3ca1f8d45ae52565b8d217d8a9e13854abf1
Vadim Akhmatov <vadim.akh42@gmail.com>
2023-07-23 19:47:13 +0700

    Use unspecified DateTime for Postgres database

---
+++ VetClinicApi.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetClinicApi.Core.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime ToUtcKind(this DateTime dateTime) =>
+        dateTime.ToKind(DateTimeKind.Utc);
+
+        public static DateTime? ToUtcKind(this DateTime? dateTime) =>
+            dateTime.ToKind(DateTimeKind.Utc);
+
+        public static DateTime ToUnspecifiedKind(this DateTime dateTime) =>
+            dateTime.ToKind(DateTimeKind.Unspecified);
+
+        public static DateTime? ToUnspecifiedKind(this DateTime? dateTime) =>
+            dateTime.ToKind(DateTimeKind.Unspecified);
+
+        public static DateTime ToKind(this DateTime dateTime, DateTimeKind kind) =>
+            DateTime.SpecifyKind(dateTime, kind);
+
+        public static DateTime? ToKind(this DateTime? dateTime, DateTimeKind kind) =>
+            dateTime?.ToKind(kind);
+    }
+}
--- VetClinicApi.Database/VetClinicContext.cs
+++ VetClinicApi.Database/VetClinicContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 using VetClinicApi.Core.Entities;
+using VetClinicApi.Core.Extensions;
 
 namespace VetClinicApi.Database
 {
@@ -9,13 +10,34 @@ namespace VetClinicApi.Database
         public virtual DbSet<Customer> Customers { get; set; }
         public VetClinicContext(DbContextOptions<VetClinicContext> contextOptions) : base(contextOptions)
         {
-            
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Customer>()
                 .ToTable("T_Customer")
                 .HasKey(x => x.Id);
+
+            modelBuilder.Entity<Customer>()
+                .Property(x => x.BirthDate)
+                    .HasConversion(
+                        src => src.ToUnspecifiedKind(),
+                        dst => dst.ToUnspecifiedKind());
+            modelBuilder.Entity<Customer>()
+                .Property(x => x.RegistrationDate)
+                    .HasConversion(
+                        src => src.ToUnspecifiedKind(),
+                        dst => dst.ToUnspecifiedKind());
+            modelBuilder.Entity<Customer>()
+                .Property(x => x.LastEditDate)
+                    .HasConversion(
+                        src => src.ToUnspecifiedKind(),
+                        dst => dst.ToUnspecifiedKind());
+            modelBuilder.Entity<Customer>()
+                .Property(x => x.LastVisitDate)
+                    .HasConversion(
+                        src => src.ToUnspecifiedKind(),
+                        dst => dst.ToUnspecifiedKind());
             base.OnModelCreating(modelBuilder);
         }
     }
