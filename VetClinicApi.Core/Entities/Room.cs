3cf042bf8519869ab2602d9857f2ca4311adfb63
Dmitrii Korpachev <mrdecart@yandex.ru>
2023-08-09 01:55:21 +0700

    Create models

---
+++ VetClinicApi.Core/Entities/Room.cs
@@ -0,0 +1,12 @@
+﻿using VetClinicApi.Core.Enums;
+
+namespace VetClinicApi.Core.Entities
+{
+    public class Room : IEntity
+    {
+        public int Id { get; set; }
+        public RoomType RoomType { get; set; }
+        public bool IsAvailable { get; set; }
+
+    }
+}
