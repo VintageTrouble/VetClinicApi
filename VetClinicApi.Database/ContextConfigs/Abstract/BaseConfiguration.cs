e148f100f86c0187ebd95a5518cfe4405ca86536
Vadim Akhmatov <vadim.akh42@gmail.com>
2023-08-09 12:47:59 +0700

    Improve model configurationg

---
+++ VetClinicApi.Database/ContextConfigs/Abstract/BaseConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using VetClinicApi.Core.Entities;
+using VetClinicApi.Core.Extensions;
+
+namespace VetClinicApi.Database.ContextConfigs.Abstract;
+
+public abstract class BaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+    where TEntity : class, IEntity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        var dateTimes = typeof(TEntity)
+            .GetProperties()
+            .Where(x => x.PropertyType == typeof(DateTime));
+
+        foreach (var item in dateTimes)
+        {
+            builder
+                .Property<DateTime>(item.Name)
+                    .HasConversion(
+                        src => src.ToUnspecifiedKind(),
+                        dst => dst.ToUnspecifiedKind());
+        }
+    }
+}
---
+++ VetClinicApi.Database/ContextConfigs/AnimalConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using VetClinicApi.Core.Entities;
+using VetClinicApi.Database.ContextConfigs.Abstract;
+
+namespace VetClinicApi.Database.ContextConfigs;
+
+public class AnimalConfiguration : BaseConfiguration<Animal>
+{
+    public override void Configure(EntityTypeBuilder<Animal> builder)
+    {
+        builder
+            .ToTable("T_Animal")
+            .HasKey(x => x.Id);
+
+        builder
+            .HasOne(x => x.Customer)
+            .WithMany()
+            .HasForeignKey(x => x.CustomerId);
+        builder
+           .HasOne(x => x.AnimalType)
+           .WithMany()
+           .HasForeignKey(x => x.AnimalTypeId);
+
+        base.Configure(builder);
+    }
+}
