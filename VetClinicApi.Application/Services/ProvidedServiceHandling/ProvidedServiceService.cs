4bc39afd2378a57394dee62388ac06fe097459f1
Dmitrii Korpachev <mrdecart@yandex.ru>
2023-08-10 14:08:16 +0700

    CleanUp

--- VetClinicApi.Application/Services/ProvidedServiceHandling/ProvidedServiceService.cs
+++ VetClinicApi.Application/Services/ProvidedServiceHandling/ProvidedServiceService.cs
@@ -2,56 +2,55 @@
 using VetClinicApi.Core.Entities;
 using VetClinicApi.Database.Repositories;
 
-namespace VetClinicApi.Application.Services.ProvidedServiceHandling
+namespace VetClinicApi.Application.Services.ProvidedServiceHandling;
+
+public class ProvidedServiceService : IProvidedServiceService
 {
-    public class ProvidedServiceService : IProvidedServiceService
+    private readonly IProvidedServiceRepository _providedServiceRepository;
+
+    public ProvidedServiceService(IProvidedServiceRepository providedServiceRepository)
     {
-        private readonly IProvidedServiceRepository _providedServiceRepository;
+        _providedServiceRepository = providedServiceRepository;
+    }
 
-        public ProvidedServiceService(IProvidedServiceRepository providedServiceRepository)
+    public async Task<ProvidedService> CreateProvidedService(ProvidedService? providedService)
+    {
+        if (providedService == null)
+            throw new ArgumentNullException(nameof(providedService), "ProvidedService can't be null.");
+
+        return await _providedServiceRepository.Add(providedService);
+    }
+
+    public async Task DeleteProvidedService(int id)
+    {
+        try
         {
-            _providedServiceRepository = providedServiceRepository;
+            await _providedServiceRepository.Delete(id);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            throw new EntityNotFoundException(id, "ProvidedService");
+        }
+    }
+
+    public async Task<IEnumerable<ProvidedService>> GetAllProvidedServices()
+    {
+        return await _providedServiceRepository.GetAll();
+    }
+
+    public async Task<ProvidedService> UpdateProvidedService(ProvidedService? providedService)
+    {
+        if (providedService == null)
+            throw new ArgumentNullException(nameof(providedService), "ProvidedService can't be null.");
+
+        try
+        {
+            return await _providedServiceRepository.Update(providedService);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            throw new EntityNotFoundException(providedService.Id, "ProvidedService");
         }
 
-        public async Task<ProvidedService> CreateProvidedService(ProvidedService? providedService)
-        {
-            if (providedService == null)
-                throw new ArgumentNullException(nameof(providedService), "ProvidedService can't be null.");
-
-            return await _providedServiceRepository.Add(providedService);
-        }
-
-        public async Task DeleteProvidedService(int id)
-        {
-            try
-            {
-                await _providedServiceRepository.Delete(id);
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-                throw new EntityNotFoundException(id, "ProvidedService");
-            }
-        }
-
-        public async Task<IEnumerable<ProvidedService>> GetAllProvidedServices()
-        {
-            return await _providedServiceRepository.GetAll();
-        }
-
-        public async Task<ProvidedService> UpdateProvidedService(ProvidedService? providedService)
-        {
-            if (providedService == null)
-                throw new ArgumentNullException(nameof(providedService), "ProvidedService can't be null.");
-
-            try
-            {
-                return await _providedServiceRepository.Update(providedService);
-            }
-            catch (ArgumentOutOfRangeException)
-            {
-                throw new EntityNotFoundException(providedService.Id, "ProvidedService");
-            }
-
-        }
     }
 }
