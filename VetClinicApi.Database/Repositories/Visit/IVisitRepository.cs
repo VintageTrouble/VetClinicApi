0a562b3a10719d09451c6893ad02b6d69a5df25e
Dmitrii Korpachev <mrdecart@yandex.ru>
2023-08-15 21:33:00 +0700

    Improve visit reposiory with GetAllByFilter method

--- VetClinicApi.Database/Repositories/Visit/IVisitRepository.cs
+++ VetClinicApi.Database/Repositories/Visit/IVisitRepository.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using VetClinicApi.Core.Entities;
+﻿using VetClinicApi.Core.Entities;
 
 namespace VetClinicApi.Database.Repositories;
 
 public interface IVisitRepository : IAbstractRepository<Visit>
 {
+    Task<IEnumerable<Visit>> GetAllByFilter(DateTime dateTime);
 }
--- VetClinicApi.Database/Repositories/Visit/VisitRepository.cs
+++ VetClinicApi.Database/Repositories/Visit/VisitRepository.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using VetClinicApi.Core.Entities;
+using VetClinicApi.Core.Enums;
 
 namespace VetClinicApi.Database.Repositories;
 
@@ -14,5 +9,13 @@ public class VisitRepository : AbstractRepository<Visit>, IVisitRepository
 {
     public VisitRepository(IDbContextFactory<VetClinicContext> contextFactory) : base(contextFactory)
     {
+    }
+
+    public async Task<IEnumerable<Visit>> GetAllByFilter(DateTime dateTime)
+    {
+        using var context = _contextFactory.CreateDbContext();
+        return await context.Visits
+           .Where(x => x.VisitDate == dateTime && x.VisitStatus == VisitStatus.Waiting)
+           .ToListAsync();
     }
 }
