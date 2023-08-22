98bd5bc1c16dbd910d8a7fe2ba6e65eac1d7397f
Vadim Akhmatov <vadim.akh42@gmail.com>
2023-08-22 19:18:00 +0700

    Cleanup

--- VetClinicApi.API.Tests/Common/Validation/CustomerValidatorTests/TestData_CustomerRequests.cs
+++ VetClinicApi.API.Tests/Common/Validation/CustomerValidatorTests/TestData_CustomerRequests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using VetClinicApi.Contracts.CustomerContracts;
+﻿using VetClinicApi.Contracts.CustomerContracts;
 
 namespace VetClinicApi.API.Tests.Common.Validation.CustomerValidatorTests;
 
